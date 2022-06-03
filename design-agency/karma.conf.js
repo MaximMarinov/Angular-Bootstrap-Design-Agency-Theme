fea7db33421fa89cee54cd2337382ce3b29ed00f
MaximMarinov <maximmarinovv@gmail.com>
2022-06-03 18:33:20 +0300

    Renamed project

--- design-agency/karma.conf.js
+++ design-agency/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/desing-agency'),
+      dir: require('path').join(__dirname, './coverage/design-agency'),
       subdir: '.',
       reporters: [
         { type: 'html' },
