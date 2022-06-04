ad7883894594c99ea92f308a590ebf904ceeae22
MaximMarinov <maximmarinovv@gmail.com>
2022-06-04 18:09:24 +0300

    Portfolio section done

--- design-agency/src/app/app-routing.module.ts
+++ design-agency/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './feature/pages/home/home.component';
+import { PortfolioComponent } from './feature/pages/portfolio/portfolio.component';
 import { ServicesComponent } from './feature/pages/services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'services', component: ServicesComponent },
+  { path: 'portfolio', component: PortfolioComponent },
 ];
 
 @NgModule({
--- design-agency/src/app/feature/feature.module.ts
+++ design-agency/src/app/feature/feature.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { RouterModule } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ServicesComponent
+    ServicesComponent,
+    PortfolioComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HomeComponent,
     ServicesComponent,
-    RouterModule
+    PortfolioComponent,
   ]
 })
 export class FeatureModule { }
